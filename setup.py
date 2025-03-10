8be85bb6ba57427f18036992a3521397cfe48324
krisanu801 <krisanusarkar03@gmail.com>
2025-03-10 14:17:13 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
-    name="sl3",
+    name="OptiHedge",
     version="0.1.0",
     author="Krisanu801",
     author_email="krisanusarkar03@gmail.com",
